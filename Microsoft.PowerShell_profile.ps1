947924ebfe8b3ba890290969e1287b2c2fb3a858
Matt Siedenburg <masieden@gmail.com>
2021-10-15 20:57:57 -0700

    Initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+oh-my-posh --init --shell pwsh --config C:\Users\Matt\AppData\Local\Programs\oh-my-posh\themes\custom.omp.json | Invoke-Expression
+Import-Module -Name Terminal-Icons
+Set-Alias -Name ll -Value ls
