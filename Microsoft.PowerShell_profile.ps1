97c01dd6f6fcf177cd4c8109bc2b169a40073bf8
Siedenburg <msiedenburg@illumina.com>
2021-10-18 14:36:26 -0700

    Added menu complete and fixed cursor on history selection

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,11 @@
 oh-my-posh --init --shell pwsh --config C:\Users\Matt\AppData\Local\Programs\oh-my-posh\themes\custom.omp.json | Invoke-Expression
+
 Import-Module -Name Terminal-Icons
-Set-Alias -Name ll -Value ls
+Import-Module PSReadLine
+
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+Set-Alias -Name 'll' -Value 'Get-ChildItem'
