5ddcec05d316cb9db95c3689cf990fb5d2c348aa
Matt Siedenburg <masieden@gmail.com>
2021-11-11 19:44:17 -0800

    Updated profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,14 @@
-oh-my-posh --init --shell pwsh --config C:\Users\Matt\AppData\Local\Programs\oh-my-posh\themes\custom.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config C:\Users\Matt\AppData\Local\Programs\oh-my-posh\themes\jandedobbeleer.omp.json | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 Import-Module PSReadLine
 
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
 Set-Alias -Name 'll' -Value 'Get-ChildItem'
